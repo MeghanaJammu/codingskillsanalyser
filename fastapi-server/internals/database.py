8768cb54be87d28ea74c6e4f85319c986e43e84a
MeghanaJammu <jammumeghana749@gmail.com>
2025-09-12 00:04:54 +0530

    ready for deployment

--- fastapi-server/internals/database.py
+++ fastapi-server/internals/database.py
@@ -1,16 +1,28 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#CREATING DATABASE CONNECTION
+load_dotenv()
+print("ENV DB_URL:", os.getenv("DB_URL"))
 
-#1. creating engine
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./app.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+# 1. Load DB URL from environment variable (fallback to SQLite for local dev)
+SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL", "sqlite:///./app.db")
+print("Connected to DB:", SQLALCHEMY_DATABASE_URL)
 
-#2.declare a mapping
+
+# 2. Create engine
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# 3. Declare base
 Base = declarative_base()
 
-#3.creating session
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+# 4. Create session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
